6d8a83d48b03e8ce108b247bf6a5bbb772cf7aec
lk26129226creator <lk26129226@gmail.com>
2025-09-30 06:20:21 +0000

    更新

--- temp_start_server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-pushd "%~dp0backend"
-set JAVA_CMD=java
-set BIN_DIR=%CD%\bin
-set LIB_DIR=%CD%\lib\*
-echo Starting server with BIN_DIR=%BIN_DIR% and LIB_DIR=%LIB_DIR%
-"%JAVA_CMD%" -cp "%BIN_DIR%;%CD%\lib\*" Server
-popd
